c3003c24cac26d15e56c27a23c9ebd68bae8847a
Guilherme <simaogui28@gmail.com>
2026-08-31 17:19:39 -0300

    fix: correct match_manager property names trecos_used_this_round and energy in TavernTable

--- scripts/ui/TavernTable.gd
+++ scripts/ui/TavernTable.gd
@@ -116,8 +116,8 @@ func _build_treco_modal_tiles() -> void:
 	for child in treco_grid.get_children():
 		child.queue_free()
 	
-	var already_used = match_manager.treco_used_this_hand[0]
-	var cur_energy = match_manager.player_energy[0]
+	var already_used = match_manager.trecos_used_this_round[0] > 0
+	var cur_energy = match_manager.energy[0]
 	
 	for treco in round_trecos:
 		var card_panel = Panel.new()
